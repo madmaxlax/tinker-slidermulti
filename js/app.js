26b7a3b6eb8fe6e70211d553a08d72dea452171e
Maxwell Struever <maxstruever@gmail.com>
2015-03-30 09:35:19 -0400

    works but not in a nice dual binding angular way

--- js/app.js
+++ js/app.js
@@ -1,14 +1,48 @@
+//angular
+(function() {
+	var sliderLoaded = false;
+	var app = angular.module('slider', []);
+	app.controller('SliderController', function() {
+		this.slidervalues = function(){
+			if(sliderLoaded)
+			{
+				return $("#slider").slider("values");
+			}
+			else
+			{
+				return "loading...";
+			}
+		};
+	});
+	var globalslidervals = [];
+	app.directive('wtwSlider', function() {
+		return {
+			restrict: 'A',//only seems to work as an attribute
+			link: function(scope, element, attrs) {
+				//apply the jquery UI Slider widget to the passed info
+				$(element).slider({
+					animate: true,
+					values: [10, 20, 30, 80],
+					slide: function(event, slider) {
+						$("#valuesoutput").text($("#slider").slider("values"));
+					},
+					create:function(){//hide loader
+						$(this).find(".slider-loader").remove();
+						sliderLoaded = true;						
+					}
+				});
+			}
+		};
+	});
+})(); //end javascript wrapping 
+
+
+
 var sliderVar;
 
-$("#slider").slider({
-	animate: true,
-	values: [10, 20, 30, 80],
-	slide: function(event, slider) {
-		$("#valuesoutput").text(slider.values.toString());
-	}
-});
+//$("#slider")
 $("#btn-add").click(function() {
-	$("#slider").slider("addHandle",70);
+	$("#slider").slider("addHandle", 70);
 	$("#slider").slider("refresh");
 
 });
@@ -16,5 +50,3 @@ $("#btn-add").click(function() {
 $("#btn-remove").click(function() {
 	$("#slider").slider("removeHandle");
 });
-
-//angular
