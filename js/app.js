e2ae1f283c4a7af28284418246e198bc6247a36d
Maxwell Struever <maxstruever@gmail.com>
2015-04-17 07:30:32 -0400

    not updating values and not sliding

--- js/app.js
+++ js/app.js
@@ -1,34 +1,44 @@
 //angular
 (function() {
-	var sliderLoaded = false;
 	var app = angular.module('slider', []);
 	app.controller('SliderController', function() {
-		this.slidervalues = function(){
-			if(sliderLoaded)
+		this.sliderLoaded = false;
+		this.vals = [];
+		this.updateVals = function(){
+			if(this.sliderLoaded)
 			{
-				return $("#slider").slider("values");
+				this.vals = $("#slider").slider("values");
 			}
 			else
 			{
-				return "loading...";
+				this.vals = "loading...";
 			}
 		};
 	});
-	var globalslidervals = [];
 	app.directive('wtwSlider', function() {
 		return {
 			restrict: 'A',//only seems to work as an attribute
 			link: function(scope, element, attrs) {
 				//apply the jquery UI Slider widget to the passed info
+				var vals = [];
+				vals = JSON.parse(attrs.wtwSlider);
+				if (vals.length === 0)
+				{
+					//default val
+					vals= [10, 20, 30, 80];
+				}
 				$(element).slider({
 					animate: true,
-					values: [10, 20, 30, 80],
+					values: vals,
 					slide: function(event, slider) {
-						//$("#valuesoutput").text($("#slider").slider("values"));
+						//scope.tempslider.vals = $(this).slider("values");
+						//scope.tempslider.updateVals();
+						//scope.$apply();
 					},
 					create:function(){//hide loader
 						$(this).find(".slider-loader").remove();
-						sliderLoaded = true;						
+						scope.tempslider.sliderLoaded = true;	
+						scope.tempslider.vals = $(this).slider("values");
 					}
 				});
 			}
@@ -37,14 +47,10 @@
 })(); //end javascript wrapping 
 
 
-
-var sliderVar;
-
 //$("#slider")
 $("#btn-add").click(function() {
 	$("#slider").slider("addHandle", 70);
 	$("#slider").slider("refresh");
-
 });
 
 $("#btn-remove").click(function() {
